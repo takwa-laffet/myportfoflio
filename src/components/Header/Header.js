a742b2af2ad70bb09e68461c1f81817b4e3ab854
takwa-laffet <laffet@kebili.r-iset.tn>
2024-11-05 20:31:34 +0100

    changemen

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
       <header>
         <h1  className="header-title"> Hello! it's <span className="purple-text">takwa laffet</span><img src={hi} style={{width:"48px",height:"48px"}}></img></h1>
         <div className="header-title-box">
-        👩‍💻 full stack developer && cybersecurite Engineering Student form tunisia
+        👩‍💻 full stack developer && cybersecurity Engineering Student form tunisia
         </div>
       </header>
     </section>
